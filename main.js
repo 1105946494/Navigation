d0298b33a7cf6f3ca1f8aeacf1d7bcbf92c5124c
zheng <1105946494@qq.com>
2021-10-15 00:15:02 +0800

    加一点小功能

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+/*加载时时间设置 */
 window.onload = function () {
   setTime();
   let time = setInterval(function () {
@@ -17,6 +18,7 @@ function setTime() {
   document.querySelector(".shi").innerHTML = shi;
   document.querySelector(".fen").innerHTML = fen;
 }
+/*input样式设置*/
 let inputDown = document.querySelector(".headerInput");
 inputDown.onclick = function () {
   inputDown.classList.add("inputDown");
@@ -26,3 +28,13 @@ inputDown.onblur = function () {
   inputDown.classList.remove("inputDown");
   inputDown.setAttribute("placeholder", "Search");
 };
+/*时间样式设置*/
+let time = document.querySelector(".time");
+time.onmousemove = function () {
+  time.classList.remove("timeMoveOut");
+  time.classList.add("timeMove");
+};
+time.onmouseout = function () {
+  time.classList.add("timeMoveOut");
+};
+/**********************************/
