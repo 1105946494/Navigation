8c5ce6e2c87a259e274d8763755a93c3a4c8e663
zheng <1105946494@qq.com>
2021-10-15 22:12:06 +0800

    用jQuery

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*加载时时间设置 */
+/*加载时间设置 */
 window.onload = function () {
   setTime();
   let time = setInterval(function () {
@@ -15,26 +15,24 @@ function setTime() {
   if (fen < 10) {
     fen = "0" + fen.toString();
   }
-  document.querySelector(".shi").innerHTML = shi;
-  document.querySelector(".fen").innerHTML = fen;
+  $(".shi").html(shi);
+  $(".fen").html(fen);
 }
 /*input样式设置*/
-let inputDown = document.querySelector(".headerInput");
-inputDown.onclick = function () {
-  inputDown.classList.add("inputDown");
-  inputDown.setAttribute("placeholder", "");
-};
-inputDown.onblur = function () {
-  inputDown.classList.remove("inputDown");
-  inputDown.setAttribute("placeholder", "Search");
-};
+$(".headerInput").click(function () {
+  $(".headerInput").addClass("inputDown");
+  $(".headerInput").attr("placeholder", "");
+});
+$(".headerInput").blur(function () {
+  $(".headerInput").removeClass("inputDown");
+  $(".headerInput").attr("placeholder", "Search");
+});
 /*时间样式设置*/
-let time = document.querySelector(".time");
-time.onmousemove = function () {
-  time.classList.remove("timeMoveOut");
-  time.classList.add("timeMove");
-};
-time.onmouseout = function () {
-  time.classList.add("timeMoveOut");
-};
+$(".time").mousemove(function () {
+  $(".time").removeClass("timeMoveOut");
+  $(".time").addClass("timeMove");
+});
+$(".time").mouseout(function () {
+  $(".time").addClass("timeMoveOut");
+});
 /**********************************/
